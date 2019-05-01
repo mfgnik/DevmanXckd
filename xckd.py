8b27f13c569f9ae47565fae8fc8c6cf9e4def67e
Mikhail Fedorov <misha17061995@yandex.ru>
2019-05-01 13:01:43 +0300

    Reformat code

--- xckd.py
+++ xckd.py
@@ -6,58 +6,73 @@ from random import randint
 
 def fetch_image(url):
     response = requests.get(url).json()
-    title = '{}.png'.format(response['title'])
-    comment = response['alt']
-    with open(title, 'wb') as f:
-        f.write(requests.get(response['img']).content)
+    try:
+        title = '{}.png'.format(response['title'])
+        comment = response['alt']
+        with open(title, 'wb') as f:
+            f.write(requests.get(response['img']).content)
+    except KeyError:
+        raise requests.exceptions.HTTPError
     return title, comment
 
 
 def get_upload_url(access_token, group_id):
-    return requests.get('https://api.vk.com/method/photos.getWallUploadServer', params={
-        'access_token': access_token,
-        'v': 5.95,
-        'group_id': group_id}
-                              ).json()['response']['upload_url']
+    try:
+        return requests.get('https://api.vk.com/method/photos.getWallUploadServer', params={
+            'access_token': access_token,
+            'v': 5.95,
+            'group_id': group_id}
+                            ).json()['response']['upload_url']
+    except KeyError:
+        raise requests.exceptions.HTTPError
 
 
 def upload_photo(access_token, upload_url):
     with open(title, 'rb') as photo_file:
         files = {'photo': photo_file}
         response = requests.post(upload_url, files=files, params={'access_token': access_token}).json()
-    return response['server'], response['photo'], response['hash']
+    try:
+        return response['server'], response['photo'], response['hash']
+    except KeyError:
+        raise requests.exceptions.HTTPError
 
 
-def save_wall_photo(access_token, photo, server, hash):
+def save_wall_photo(access_token, server, photo, image_hash):
     response = requests.post('https://api.vk.com/method/photos.saveWallPhoto', data={
         'photo': photo,
         'server': server,
-        'hash': hash}, params={
+        'hash': image_hash}, params={
         'access_token': access_token,
         'v': 5.95,
         'group_id': group_id}
                              ).json()['response'][0]
-    return response['id'], response['owner_id']
+    try:
+        return response['id'], response['owner_id']
+    except KeyError:
+        raise requests.exceptions.HTTPError
 
 
 def wall_post(access_token, group_id, media_id, owner_id):
-    requests.get('https://api.vk.com/method/wall.post', params={
+    response = requests.get('https://api.vk.com/method/wall.post', params={
         'access_token': access_token,
         'v': 5.95,
         'owner_id': -group_id,
         'from_group': 1,
         'attachments': 'photo{}_{}'.format(owner_id, media_id),
-        'message': comment})
+        'message': comment}).json()
+    print(response)
+    if 'error' in response:
+        raise requests.exceptions.HTTPError
 
 
 if __name__ == '__main__':
     load_dotenv()
     access_token = os.getenv('access_token')
     group_id = int(os.getenv('group_id'))
-    amount_of_comics = requests.get('http://xkcd.com/info.0.json').json()['num']
-    title, comment = fetch_image('http://xkcd.com/{}/info.0.json'.format(randint(1, amount_of_comics)))
-    upload_url = get_upload_url(access_token, group_id)
-    server, photo, hash = upload_photo(access_token, upload_url)
-    media_id, owner_id = save_wall_photo(access_token, photo, server, hash)
-    wall_post(access_token, group_id, media_id, owner_id)
-    os.remove(title)
+    try:
+        amount_of_comics = requests.get('http://xkcd.com/info.0.json').json()['num']
+        title, comment = fetch_image('http://xkcd.com/{}/info.0.json'.format(randint(1, amount_of_comics)))
+        wall_post(access_token, group_id,
+                  *save_wall_photo(access_token, *upload_photo(access_token, get_upload_url(access_token, group_id))))
+    finally:
+        os.remove(title)
